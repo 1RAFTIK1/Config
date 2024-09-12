c849c50b93c2626296c322ca575419d9d4a8263b
1RAFTIK1 <134624191+1RAFTIK1@users.noreply.github.com>
2024-09-12 22:11:12 +0300

    Update main.py
    few func

--- main.py
+++ main.py
@@ -18,4 +18,44 @@ def ls (path, files):
             if len(file_names) > 1 or not file_names:
                 continue
             print("\033[33m{}\033[0m".format(file_names[0]))
+def cd(path, extension_path, files):
+    if "root:" in extension_path:
+        path = extension_path[len("root:")]
+    else:
+        path += "/" + extension_path
+    path = delete_symbol(path)
 
+    global local_path
+
+    if path == "":
+        local_path = ""
+        return True
+
+    if "../" in path:
+        local_path = local_path[:len(local_path) - len(local_path.split("/")[-1]) - 1]
+        return True
+
+    for file in files:
+        if path in file.filename:
+            local_path = "/" + path
+            return True
+    return False
+
+
+def cat(path, extension_path, zip_file):
+    if "root:" in extension_path:
+        path = extension_path[len("root:")]
+    else:
+        path += "/" + extension_path
+    path = delete_symbol(path)
+
+    flag = False
+    for file in ZipFile(zip_file).filelist:
+        if path in file.filename:
+            flag = True
+            with ZipFile(zip_file) as files:
+                with files.open(path,'r') as file:
+                    for line in file.readlines():
+                        print(line.decode('utf8').strip())
+    if not flag:
+        print("\033[33m{}\033[0m".format("Can`t open this file"))
